44c950602049c1007eac980dd5796fb5791c8f86
A10s <aaaa@ya.ru>
2022-07-07 22:25:39 +0300

    polz

---
+++ polz/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "anna")
+{
+     Console.WriteLine ("O, ЖОПА, давно тебя ждем!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.Write (username);
+    Console.Write (", мы ждем тут ЖОПУ, а не тебя!!!");
+}
