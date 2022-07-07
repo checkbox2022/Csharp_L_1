100268e296ac47813a750754e1a2c578deb9b144
A10s <aaaa@ya.ru>
2022-07-07 22:11:37 +0300

    sddfsdfsdfsdfsd

---
+++ razn/Program.cs
@@ -0,0 +1,7 @@
+﻿double a = new Random().Next(10,50);
+Console.Write("a\t");
+Console.WriteLine(a);
+double b = new Random().Next(65,90);
+Console.Write("b\t");
+Console.WriteLine(b);
+Console.WriteLine(a + b);
